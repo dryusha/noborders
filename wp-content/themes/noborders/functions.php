3ac647dc40a5fe8dce4b09f64a9c3893878f3e96
bboyandry <dryshabot>
2021-06-02 09:15:56 +0300

    add new plugins with accordion add thumbnail for posts

--- wp-content/themes/noborders/functions.php
+++ wp-content/themes/noborders/functions.php
@@ -1,5 +1,5 @@
 <?php
-function noborders_theme_support() {
+function noborders_theme_setup() {
     /*
 	 * Let WordPress manage the document title.
 	 * By adding theme support, we declare that this theme does not use a
@@ -7,12 +7,35 @@ function noborders_theme_support() {
 	 * provide it for us.
 	 */
     add_theme_support( 'title-tag' );
+
+    /*
+     * Enable support for Post Thumbnails on posts and pages.
+     *
+     * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+     */
+    add_theme_support( 'post-thumbnails' );
 }
 
 function careerPosts() {
     $args = array( 'posts_per_page' => 10, 'category_name' => 'careers');
     $careers_posts = new WP_Query($args);
-    $content = '<h3>Open positions</h3>';
+    $content = "<h3>Open positions</h3>";
+
+    while($careers_posts->have_posts()) :
+        $careers_posts->the_post();
+
+        $content .= "<div>".get_the_title()."</div>";
+        $content .= "<div><a href='".get_permalink()."'>Read More</a></div>";
+
+    endwhile;
+
+    return $content;
+}
+
+function portfolioPosts() {
+    $args = array( 'posts_per_page' => 10, 'category_name' => 'portfolio');
+    $careers_posts = new WP_Query($args);
+    $content = '';
 
     while($careers_posts->have_posts()) :
         $careers_posts->the_post();
@@ -26,4 +49,4 @@ function careerPosts() {
 }
 
 add_shortcode('careerPosts', 'careerPosts' );
-add_action( 'after_setup_theme', 'noborders_theme_support' );
+add_action( 'after_setup_theme', 'noborders_theme_setup' );
