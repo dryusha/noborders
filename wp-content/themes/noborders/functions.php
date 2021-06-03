e7672a884d2c3c8fcc7a1fd6ed6d8be81be08f17
andrey.savotchenko <dryshabot>
2021-06-03 10:42:12 +0300

    add logo + menu

--- wp-content/themes/noborders/functions.php
+++ wp-content/themes/noborders/functions.php
@@ -1,5 +1,26 @@
 <?php
 function noborders_theme_setup() {
+
+    // Custom logo.
+    $logo_width  = 120;
+    $logo_height = 90;
+
+    // If the retina setting is active, double the recommended width and height.
+    if ( get_theme_mod( 'retina_logo', false ) ) {
+        $logo_width  = floor( $logo_width * 2 );
+        $logo_height = floor( $logo_height * 2 );
+    }
+
+    add_theme_support(
+        'custom-logo',
+        array(
+            'height'      => $logo_height,
+            'width'       => $logo_width,
+            'flex-height' => true,
+            'flex-width'  => true,
+        )
+    );
+
     /*
 	 * Let WordPress manage the document title.
 	 * By adding theme support, we declare that this theme does not use a
@@ -14,7 +35,28 @@ function noborders_theme_setup() {
      * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
      */
     add_theme_support( 'post-thumbnails' );
+
+    add_shortcode('portfolioPosts', 'portfolioPosts' );
+    add_shortcode('careerPosts', 'careerPosts' );
 }
+
+/**
+ * Register navigation menus uses wp_nav_menu in five places.
+ */
+function noborders_theme_menus() {
+
+    $locations = array(
+        'primary'  => __( 'Desktop Horizontal Menu', 'twentytwenty' ),
+        'expanded' => __( 'Desktop Expanded Menu', 'twentytwenty' ),
+        'mobile'   => __( 'Mobile Menu', 'twentytwenty' ),
+        'footer'   => __( 'Footer Menu', 'twentytwenty' ),
+        'social'   => __( 'Social Menu', 'twentytwenty' ),
+    );
+
+    register_nav_menus( $locations );
+}
+
+add_action( 'init', 'noborders_theme_menus' );
 
 function careerPosts() {
     $args = array( 'posts_per_page' => 10, 'category_name' => 'careers');
@@ -32,21 +74,20 @@ function careerPosts() {
     return $content;
 }
 
-function portfolioPosts() {
-    $args = array( 'posts_per_page' => 10, 'category_name' => 'portfolio');
+function portfolioPosts($atts) {
+    $args = array( 'posts_per_page' => $atts["limit"], 'category_name' => 'portfolio');
     $careers_posts = new WP_Query($args);
     $content = '';
 
     while($careers_posts->have_posts()) :
         $careers_posts->the_post();
 
+        $content .= "<div><a href='".get_permalink()."'>".get_the_post_thumbnail()."</a></div>";
         $content .= "<div>".get_the_title()."</div>";
-        $content .= "<div><a href='".get_permalink()."'>Read More</a></div>";
 
     endwhile;
 
     return $content;
 }
 
-add_shortcode('careerPosts', 'careerPosts' );
 add_action( 'after_setup_theme', 'noborders_theme_setup' );
--- wp-content/themes/noborders/header.php
+++ wp-content/themes/noborders/header.php
@@ -9,3 +9,53 @@
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
+
+    <header>
+        <?php
+            if(has_custom_logo()) {
+        ?>
+            <div><?php
+
+                $logo = get_theme_mod( 'custom_logo' );
+                $image = wp_get_attachment_image_src( $logo , 'full' );
+                $image_url = $image[0];
+                $image_width = $image[1];
+                $image_height = $image[2];
+                ?>
+
+                <img src="<?php echo $image_url;?>" alt="">
+            </div>
+        <?php
+            }
+        ?>
+
+
+        <?php
+		    if ( has_nav_menu( 'expanded' ) ) {
+		?>
+            <div class="toggle-wrapper nav-toggle-wrapper has-expanded-menu">
+
+                <button class="toggle nav-toggle desktop-nav-toggle" data-toggle-target=".menu-modal" data-toggle-body-class="showing-menu-modal" aria-expanded="false" data-set-focus=".close-nav-toggle">
+                    <span class="toggle-inner">
+                        <span class="toggle-text"><?php _e( 'Menu', 'noborders' ); ?></span>
+                        <span class="toggle-icon">
+                            <?php
+                                /*twentytwenty_the_theme_svg( 'ellipsis' );*/
+                                wp_nav_menu(
+                                    array(
+                                        'container'  => '',
+                                        'items_wrap' => '%3$s',
+                                        'theme_location' => 'expanded',
+                                    )
+                                );
+                            ?>
+
+                        </span>
+                    </span>
+                </button><!-- .nav-toggle -->
+
+            </div><!-- .nav-toggle-wrapper -->
+        <?php
+            }
+        ?>
+    </header>
